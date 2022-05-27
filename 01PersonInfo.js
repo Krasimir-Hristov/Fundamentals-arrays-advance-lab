979be717aed38934ae87d07f4fe2ee8d76fa6ef6
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-27 17:04:16 +0200

    Add files via upload

---
+++ 01PersonInfo.js
@@ -0,0 +1,16 @@
+function solve(firstName,lastName,age){
+
+    let person = {
+        firstName: firstName,
+        lastName: lastName,
+         age: age
+    };
+
+    return person
+   
+
+
+}
+solve("Peter", 
+"Pan",
+"20")
